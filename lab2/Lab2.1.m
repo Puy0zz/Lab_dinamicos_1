36ec5864c47420c8c184539475bf5dd1f66afed7
luisaristi1 <141964513+luisaristi1@users.noreply.github.com>
2024-08-29 22:45:06 -0500

    Update Lab2.1.m

--- lab2/Lab2.1.m
+++ lab2/Lab2.1.m
@@ -1,11 +1,18 @@
 %%Punto 1
-A=212;
-B='REConocer';
-C= 41;
-D= 'ella te da detalle';
-E= 2332;
-F= 'amor a roma';
-G= 'Amor';
+ A= 6006
+ B =7337
+ C= 289717982
+ D= 01234
+ E = 'ana lava lana';
+ F= 'se van a sus naves'; 
+ G='sor rebeca hace berros'; 
+ H='la ruta natural' ;
+ I=[1 4 6 9 ];
+ J='no traces en ese carton'; 
+ K='sometamos o matemos';
+ L=2547987452;
+ M='Yo hago yoga hoy';
+ N='La ruta nos aporto otro paso al natural';
 
 
 a=Palindromo(A)
@@ -15,6 +22,14 @@ d=Palindromo(D)
 e=Palindromo(E)
 f=Palindromo(F)
 g=Palindromo(G)
+h=Palindromo(H)
+i=Palindromo(I)
+j=Palindromo(J)
+k=Palindromo(K)
+l=Palindromo(L)
+m=Palindromo(M)
+n=Palindromo(N)
+
 
 function boolean = Palindromo(A)
    if isnumeric(A)
